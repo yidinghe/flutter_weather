5141d5e59ecf395bf850d26abc9a75ee1bb67a0a
Yiding He <yidinghe2012@gmail.com>
2018-12-14 15:19:11 -0500

    Split the background dart.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:weather/forcast/background/background_with_rings.dart';
 
 void main() => runApp(MyApp());
 
@@ -30,147 +31,10 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
-        fit: StackFit.expand,
         children: <Widget>[
-          Image.asset(
-            'assets/weather-bk_enlarged.png',
-            fit: BoxFit.cover,
-          ),
-          ClipOval(
-            clipper: CircleClipper(
-              radius: 140.0,
-              offset: Offset(40.0, 0.0),
-            ),
-            child: Image.asset(
-              'assets/weather-bk.png',
-              fit: BoxFit.cover,
-            ),
-          ),
-          CustomPaint(
-            painter: WhiteCircleCutoutPainter(
-              centerOffset: const Offset(40.0, 0.0),
-              circles: [
-                Circle(radius: 140.0, alpha: 0x10),
-                Circle(radius: 140.0 + 15.0, alpha: 0x28),
-                Circle(radius: 140.0 + 35.0, alpha: 0x38),
-                Circle(radius: 140.0 + 75.0, alpha: 0x50),
-              ],
-            ),
-            child: Container(),
-          ),
+          BackgroundWithRings(),
         ],
       ),
     );
   }
-}
-
-class CircleClipper extends CustomClipper<Rect> {
-  final double radius;
-  final Offset offset;
-
-  CircleClipper({
-    this.radius,
-    this.offset = const Offset(0.0, 0.0),
-  });
-
-  @override
-  Rect getClip(Size size) {
-    return new Rect.fromCircle(
-      center: new Offset(0.0, size.height / 2) + offset,
-      radius: radius,
-    );
-  }
-
-  @override
-  bool shouldReclip(CustomClipper<Rect> oldClipper) {
-    return true;
-  }
-}
-
-class WhiteCircleCutoutPainter extends CustomPainter {
-  final Color overlayColor = const Color(0xFFAA88AA);
-
-  final List<Circle> circles;
-  final Offset centerOffset;
-  final Paint whitePaint;
-  final Paint borderPaint;
-
-  WhiteCircleCutoutPainter({
-    this.circles = const [],
-    this.centerOffset = const Offset(0.0, 0.0),
-  })  : whitePaint = new Paint(),
-        borderPaint = new Paint() {
-    borderPaint
-      ..color = const Color(0x10FFFFFF)
-      ..style = PaintingStyle.stroke
-      ..strokeWidth = 3.0;
-  }
-
-  @override
-  void paint(Canvas canvas, Size size) {
-    for (var i = 1; i < circles.length; ++i) {
-      _maskCircle(canvas, size, circles[i - 1].radius);
-
-      whitePaint.color = overlayColor.withAlpha(circles[i - 1].alpha);
-
-      // Fill circle
-      canvas.drawCircle(
-        Offset(0.0, size.height / 2) + centerOffset,
-        circles[i].radius,
-        whitePaint,
-      );
-
-      // Draw circle bevel
-      canvas.drawCircle(
-        Offset(0.0, size.height / 2) + centerOffset,
-        circles[i - 1].radius,
-        borderPaint,
-      );
-    }
-
-    // Mask the area of the final circle
-    _maskCircle(canvas, size, circles.last.radius);
-
-    // Draw an overlay that fills the rest of the screen.
-    whitePaint.color = overlayColor.withAlpha(circles.last.alpha);
-    canvas.drawRect(
-      Rect.fromLTWH(0.0, 0.0, size.width, size.height),
-      whitePaint,
-    );
-
-    canvas.drawCircle(
-      Offset(0.0, size.height / 2) + centerOffset,
-      circles.last.radius,
-      borderPaint,
-    );
-  }
-
-  _maskCircle(Canvas canvas, Size size, double radius) {
-    Path clippedCircle = new Path();
-    clippedCircle.fillType = PathFillType.evenOdd;
-    clippedCircle.addRect(Rect.fromLTWH(0.0, 0.0, size.width, size.height));
-    clippedCircle.addOval(
-      Rect.fromCircle(
-        center: Offset(0.0, size.height / 2) + centerOffset,
-        radius: radius,
-      ),
-    );
-
-    canvas.clipPath(clippedCircle);
-  }
-
-  @override
-  bool shouldRepaint(CustomPainter oldDelegate) {
-    return true;
-  }
-}
-
-class Circle {
-  final double radius;
-  final int alpha;
-
-  Circle({
-    this.radius,
-    this.alpha = 0xFF,
-  });
 }
