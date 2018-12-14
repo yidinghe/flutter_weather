e51eb33328aa46a6b7e1d948a7912693475ba73d
Yiding He <yidinghe2012@gmail.com>
2018-12-14 14:58:23 -0500

    init commit.

---
+++ lib/main.dart
@@ -0,0 +1,176 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Weather',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        fit: StackFit.expand,
+        children: <Widget>[
+          Image.asset(
+            'assets/weather-bk_enlarged.png',
+            fit: BoxFit.cover,
+          ),
+          ClipOval(
+            clipper: CircleClipper(
+              radius: 140.0,
+              offset: Offset(40.0, 0.0),
+            ),
+            child: Image.asset(
+              'assets/weather-bk.png',
+              fit: BoxFit.cover,
+            ),
+          ),
+          CustomPaint(
+            painter: WhiteCircleCutoutPainter(
+              centerOffset: const Offset(40.0, 0.0),
+              circles: [
+                Circle(radius: 140.0, alpha: 0x10),
+                Circle(radius: 140.0 + 15.0, alpha: 0x28),
+                Circle(radius: 140.0 + 35.0, alpha: 0x38),
+                Circle(radius: 140.0 + 75.0, alpha: 0x50),
+              ],
+            ),
+            child: Container(),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class CircleClipper extends CustomClipper<Rect> {
+  final double radius;
+  final Offset offset;
+
+  CircleClipper({
+    this.radius,
+    this.offset = const Offset(0.0, 0.0),
+  });
+
+  @override
+  Rect getClip(Size size) {
+    return new Rect.fromCircle(
+      center: new Offset(0.0, size.height / 2) + offset,
+      radius: radius,
+    );
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Rect> oldClipper) {
+    return true;
+  }
+}
+
+class WhiteCircleCutoutPainter extends CustomPainter {
+  final Color overlayColor = const Color(0xFFAA88AA);
+
+  final List<Circle> circles;
+  final Offset centerOffset;
+  final Paint whitePaint;
+  final Paint borderPaint;
+
+  WhiteCircleCutoutPainter({
+    this.circles = const [],
+    this.centerOffset = const Offset(0.0, 0.0),
+  })  : whitePaint = new Paint(),
+        borderPaint = new Paint() {
+    borderPaint
+      ..color = const Color(0x10FFFFFF)
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 3.0;
+  }
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    for (var i = 1; i < circles.length; ++i) {
+      _maskCircle(canvas, size, circles[i - 1].radius);
+
+      whitePaint.color = overlayColor.withAlpha(circles[i - 1].alpha);
+
+      // Fill circle
+      canvas.drawCircle(
+        Offset(0.0, size.height / 2) + centerOffset,
+        circles[i].radius,
+        whitePaint,
+      );
+
+      // Draw circle bevel
+      canvas.drawCircle(
+        Offset(0.0, size.height / 2) + centerOffset,
+        circles[i - 1].radius,
+        borderPaint,
+      );
+    }
+
+    // Mask the area of the final circle
+    _maskCircle(canvas, size, circles.last.radius);
+
+    // Draw an overlay that fills the rest of the screen.
+    whitePaint.color = overlayColor.withAlpha(circles.last.alpha);
+    canvas.drawRect(
+      Rect.fromLTWH(0.0, 0.0, size.width, size.height),
+      whitePaint,
+    );
+
+    canvas.drawCircle(
+      Offset(0.0, size.height / 2) + centerOffset,
+      circles.last.radius,
+      borderPaint,
+    );
+  }
+
+  _maskCircle(Canvas canvas, Size size, double radius) {
+    Path clippedCircle = new Path();
+    clippedCircle.fillType = PathFillType.evenOdd;
+    clippedCircle.addRect(Rect.fromLTWH(0.0, 0.0, size.width, size.height));
+    clippedCircle.addOval(
+      Rect.fromCircle(
+        center: Offset(0.0, size.height / 2) + centerOffset,
+        radius: radius,
+      ),
+    );
+
+    canvas.clipPath(clippedCircle);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return true;
+  }
+}
+
+class Circle {
+  final double radius;
+  final int alpha;
+
+  Circle({
+    this.radius,
+    this.alpha = 0xFF,
+  });
+}
